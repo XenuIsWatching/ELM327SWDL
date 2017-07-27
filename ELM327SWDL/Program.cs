b9117ac3f3be1772b5e98d6d0436775c4242192b
Ryan McClelland <Ryan.McClelland@apollovideo.com>
2017-07-26 20:01:48 -0700

    regex refactor

--- ELM327SWDL/Program.cs
+++ ELM327SWDL/Program.cs
@@ -24,8 +24,8 @@ namespace ELM327SWDL
         public class erase
         {
             public string start_address { get; set; }
-            public List<int> data { get; set; }
             public string length { get; set; }
+            public List<int> data { get; set; }
         }
 
         public class VBF_File
@@ -38,6 +38,9 @@ namespace ELM327SWDL
             public string ecu_address { get; set; }
             public string frame_format { get; set; }
             public string file_checksum { get; set; }
+            public string call { get; set; }
+            public string omit { get; set; }
+            public string data_format_identifier { get; set; }
             public List<erase> erase_block { get; set; }
         }
 
@@ -49,41 +52,70 @@ namespace ELM327SWDL
             String vbfString = myStreamReader.ReadToEnd();
 
             //parse HEADER
-            String pattern = "vbf_version = (?<vbf_version>\\d.\\d);\\s+header {[\\S\\s]*description ?= ?{\"(?<description>.*)\"\\s*};[\\S\\s]*sw_part_number ?= ?\"(?<sw_part_number>.*)\";[\\S\\s]*sw_part_type = (?<sw_part_type>\\w+);[\\S\\s]*network ?= ?(?<network>\\w+);[\\S\\s]*ecu_address = (?<ecu_address>0[xX][0-9a-fA-F]+);[\\S\\s]*frame_format ?= ?(?<frame_format>\\w+);[\\S\\s]*erase = {(?<erase>[\\S\\s]*)};[\\S\\s]*file_checksum = (?<file_checksum>0[xX][0-9a-fA-F]+);[\\n\\r\\s]*}";
-            MatchCollection matches = Regex.Matches(vbfString, pattern);
-            Match match = matches[0];
-            VBFFile.vbf_version = match.Groups["vbf_version"].Value;
-            VBFFile.description = match.Groups["description"].Value;
-            VBFFile.sw_part_number = match.Groups["sw_part_number"].Value;
-            VBFFile.sw_part_type = match.Groups["sw_part_type"].Value;
-            VBFFile.network = match.Groups["network"].Value;
-            VBFFile.ecu_address = match.Groups["ecu_address"].Value;
-            VBFFile.frame_format = match.Groups["frame_format"].Value; 
-            VBFFile.file_checksum = match.Groups["file_checksum"].Value;
-            string data_group = match.Groups["data"].Value;
+            String pattern = "[\\S\\s]*header *\\{(?<header>((?>[^{}]+|{(?<c>)|\\}(?<-c>))*(?(c)(?!))))\\}";
+            Match header = Regex.Match(vbfString, pattern);
+            String headerString = header.Groups["header"].Value;
 
-            string erase_group = match.Groups["erase"].Value;
-            String erase_pattern = "{\\s*(?<start_address>0[xX][0-9a-fA-F]+),\\s*(?<length>0[xX][0-9a-fA-F]+)\\s*}";
-            MatchCollection erase_matches = Regex.Matches(erase_group, erase_pattern);
-            VBFFile.erase_block = new List<erase>();
+            VBFFile.vbf_version = Regex.Match(vbfString, "vbf_version *= *(?<vbf_version>\\d.\\d);").Groups["vbf_version"].Value;
+            //VBFFile.vbf_version = match.Groups["vbf_version"].Value;
 
-            FileStream sr = File.OpenRead(FileName);
-            sr.Seek(match.Length, SeekOrigin.Begin);
-            for (int i = 0; i < erase_matches.Count; i++)
+            VBFFile.description = Regex.Match(vbfString, "description *= *{\"(?<description>.*)\"\\s*};").Groups["description"].Value;
+            //VBFFile.description = match.Groups["description"].Value;
+
+            VBFFile.sw_part_number = Regex.Match(vbfString, "sw_part_number *= *\"(?<sw_part_number>.*)\";").Groups["sw_part_number"].Value;
+            //VBFFile.sw_part_number = match.Groups["sw_part_number"].Value;
+
+            VBFFile.sw_part_type = Regex.Match(vbfString, "sw_part_type *= *(?<sw_part_type>\\w+);").Groups["sw_part_type"].Value;
+            //VBFFile.sw_part_type = match.Groups["sw_part_type"].Value;
+
+            VBFFile.network = Regex.Match(vbfString, "network *= *(?<network>\\w+);").Groups["network"].Value;
+            //VBFFile.network = match.Groups["network"].Value;
+
+            VBFFile.ecu_address = Regex.Match(vbfString, "ecu_address *= *(?<ecu_address>0[xX][0-9a-fA-F]+);").Groups["ecu_address"].Value;
+            //VBFFile.ecu_address = match.Groups["ecu_address"].Value;
+
+            VBFFile.frame_format = Regex.Match(vbfString, "frame_format *= *(?<frame_format>\\w+);").Groups["frame_format"].Value;
+            //VBFFile.frame_format = match.Groups["frame_format"].Value;
+
+            VBFFile.file_checksum = Regex.Match(vbfString, "file_checksum *= *(?<file_checksum>0[xX][0-9a-fA-F]+);").Groups["file_checksum"].Value;
+            //VBFFile.file_checksum = match.Groups["file_checksum"].Value;
+
+            if (VBFFile.sw_part_type == "SBL" || VBFFile.sw_part_type == "GBL")
             {
-                erase block = new erase();
-                block.start_address = erase_matches[i].Groups["start_address"].Value;
-                block.length = erase_matches[i].Groups["length"].Value;
-                block.data = new List<int>();
-                sr.Seek(sr.Position + 4, SeekOrigin.Begin); //skip address
-                sr.Seek(sr.Position + 4, SeekOrigin.Begin); //skip length
-                for (int j = 0; j < Convert.ToInt32(block.length, 16); j++)
-                {
-                    block.data.Add(sr.ReadByte());
-                }
-                sr.Seek(sr.Position + 2, SeekOrigin.Begin); //skip checksum
-                VBFFile.erase_block.Add(block);
+                VBFFile.call = Regex.Match(vbfString, "call *= *(?<call>0[xX][0-9a-fA-F]+);").Groups["call"].Value;
             }
+            else if(VBFFile.sw_part_type == "TEST")
+            {
+                //TODO: 'attempt to find call'
+            }
+            else
+            {
+                string erase_group;
+                erase_group = Regex.Match(vbfString, "erase *= *{(?<erase>[\\S\\s]*)};").Groups["erase"].Value;
+                //string erase_group = match.Groups["erase"].Value;
+                String erase_pattern = "{\\s*(?<start_address>0[xX][0-9a-fA-F]+),\\s*(?<length>0[xX][0-9a-fA-F]+)\\s*}";
+                MatchCollection erase_matches = Regex.Matches(erase_group, erase_pattern);
+                VBFFile.erase_block = new List<erase>();
+                FileStream sr = File.OpenRead(FileName);
+                sr.Seek(header.Length, SeekOrigin.Begin);
+                for (int i = 0; i < erase_matches.Count; i++)
+                {
+                    erase block = new erase();
+                    block.start_address = erase_matches[i].Groups["start_address"].Value;
+                    block.length = erase_matches[i].Groups["length"].Value;
+                    block.data = new List<int>();
+                    sr.Seek(sr.Position + 4, SeekOrigin.Begin); //skip address
+                    sr.Seek(sr.Position + 4, SeekOrigin.Begin); //skip length
+                    for (int j = 0; j < Convert.ToInt32(block.length, 16); j++)
+                    {
+                        block.data.Add(sr.ReadByte());
+                    }
+                    sr.Seek(sr.Position + 2, SeekOrigin.Begin); //skip checksum
+                    VBFFile.erase_block.Add(block);
+                }
+            }
+
+           
 
             return VBFFile;
         }
